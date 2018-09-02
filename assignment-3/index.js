3b66e86c4b356b0e1c669f56a08c8f4041a47d58
Rens <feeling4design@gmail.com>
2018-09-02 21:48:38 +0200

    notes

--- assignment-3/index.js
+++ assignment-3/index.js
@@ -6,24 +6,16 @@
 
 
 /*
-Homework Assignment #2
+Homework Assignment #3
 
-You are building the API for a pizza-delivery company. Don't worry about a frontend, just build the API. Here's the spec from your project manager: 
+It is time to build a simple frontend for the Pizza-Delivery API you created in Homework Assignment #2. Please create a web app that allows customers to:
+1. Signup on the site
+2. View all the items available to order
+3. Fill up a shopping cart
+4. Place an order (with fake credit card credentials), and receive an email receipt
 
-1. New users can be created, their information can be edited, and they can be deleted. We should store their name, email address, and street address.
+This is an open-ended assignment. You can take any direction you'd like to go with it, as long as your project includes the requirements. It can include anything else you wish as well. 
 
-2. Users can log in and log out by creating or destroying a token.
-
-3. When a user is logged in, they should be able to GET all the possible menu items (these items can be hardcoded into the system). 
-
-4. A logged-in user should be able to fill a shopping cart with menu items
-
-5. A logged-in user should be able to create an order. 
-You should integrate with the Sandbox of Stripe.com to accept their payment. 
-Note: Use the stripe sandbox for your testing. 
-Follow this link and click on the "tokens" tab to see the fake tokens you can use server-side to confirm the integration is working: https://stripe.com/docs/testing#cards
-
-6. When an order is placed, you should email the user a receipt. You should integrate with the sandbox of Mailgun.com for this. Note: Every Mailgun account comes with a sandbox email account domain (whatever@sandbox123.mailgun.org) that you can send from by default. So, there's no need to setup any DNS for your domain for this task https://documentation.mailgun.com/en/latest/faqs.html#how-do-i-pick-a-domain-name-for-my-mailgun-account
 */
 
 // Dependencies
